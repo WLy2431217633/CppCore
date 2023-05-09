068fd6958ed79ef23c38a5352d5165ff8b33c22a
Leo Wu <2431217633@qq.com>
2023-05-09 21:34:10 +0800

    堆区new，栈区放局部常变量，全局有全局变量、静态常变量

--- 01_variable.cpp
+++ 01_variable.cpp
@@ -9,7 +9,13 @@
 */
 using namespace std;
 
-int *func()
+int * func()
+{
+    int p = 10;
+    return &p;
+}
+
+int *func1()
 {
     int *p = new int(10);
     return p;
@@ -56,27 +62,60 @@ int main()
     cout << "局部常量const_local_b的地址为:" << &const_local_b << endl;
 
 
-    // 栈区使用new开辟内存，由编译器释放
-    int *p = func();
-    cout << *p << endl;
+    // 栈区
+    int *p1 = func();
+    cout << "栈区地址：" << &p1 << endl;
+
+    delete p1;
+    cout << "栈区地址:" << &p1 << endl;
+
+
+    // 堆区使用new开辟内存，由编译器释放
+    int *p2 = func1();
+    cout << "堆区地址：" << &p2 << endl;
     /*堆区的数据由程序员管理开辟，释放；如果想释放堆区的数据用关键字delete*/
-    delete p;
-    cout << *p << endl;
+    delete p2;
+    cout << "堆区地址：" << &p2 << endl;
+
+    // 开辟数组
+    int *arr = new int[10];
+    for (int i = 0; i < 10; i++)
+        arr[i] = i+100;
+    for (int i = 0; i < 10; i++)
+        cout << arr[i] << endl;
+    // 释放数组 delete 后加[]
+    delete[] arr;
+    
+
+    
+
     /*
-    局部变量 a 的地址：0x597ddff834
-    局部变量 b 的地址：0x597ddff830
-    全局变量 global_a 的地址：0x7ff60c189010
-    全局变量 global_b 的地址：0x7ff60c189014
-    静态变量 static_a 的地址：0x7ff60c189018
-    静态变量 static_b 的地址：0x7ff60c18901c
-    字符串常量地址为：0x7ff60c18c0b3
-    字符串常量地址为：0x7ff60c18c0bf
-    全局常量const_global_a的地址为:0x7ff60c18c004
-    全局常量const_global_b的地址为:0x7ff60c18c008
-    局部常量const_local_a的地址为:0x597ddff82c
-    局部常量const_local_b的地址为:0x597ddff828
-    10
-    -2123225312
+    局部变量 a 的地址：0x4c6cdff81c
+    局部变量 b 的地址：0x4c6cdff818
+    全局变量 global_a 的地址：0x7ff6fda19010
+    全局变量 global_b 的地址：0x7ff6fda19014
+    静态变量 static_a 的地址：0x7ff6fda19018
+    静态变量 static_b 的地址：0x7ff6fda1901c
+    字符串常量地址为：0x7ff6fda1c0b3
+    字符串常量地址为：0x7ff6fda1c0bf
+    全局常量const_global_a的地址为:0x7ff6fda1c004
+    全局常量const_global_b的地址为:0x7ff6fda1c008
+    局部常量const_local_a的地址为:0x4c6cdff814
+    局部常量const_local_b的地址为:0x4c6cdff810
+    栈区地址：0x4c6cdff808
+    栈区地址:0x4c6cdff808
+    堆区地址：0x4c6cdff800
+    堆区地址：0x4c6cdff800
+    100
+    101
+    102
+    103
+    104
+    105
+    106
+    107
+    108
+    109
     */
 
 
